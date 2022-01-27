1e25384b32ff3ba147b42a21f27ce73e91d8bb22
mat9624 <mattiamango96@gmail.com>
2022-01-27 12:33:48 +0100

    funziona finalmente

--- src/main/java/com/example/ums/configuration/WebConfiguration.java
+++ src/main/java/com/example/ums/configuration/WebConfiguration.java
@@ -1,13 +1,25 @@
 package com.example.ums.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+/**
+ *
+ * @author Antonio Magnocavallo
+ * @company PIXELTEK S.R.L.
+ * @email amagnocavallo@pxltk.com
+ * */
 @Configuration
-public class WebConfiguration implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
+public class WebConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
